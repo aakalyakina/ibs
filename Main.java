6d1210a49f04b5eb754afb98038d7af76eb42dbd
aakalyakina <kalyakinaanastasia@yandex.ru>
2022-06-09 15:57:29 +0300

    1 и 2 задание

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
 
         double num1 = 90620.22;
         byte num2 = 127;
-
 
     }
 }
