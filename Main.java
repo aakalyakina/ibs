ea9e1f0b0ed0f8e34a37dafed55e72317b5a1958
aakalyakina <kalyakinaanastasia@yandex.ru>
2022-06-09 15:51:50 +0300

    2 коммит

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        double num1 = 90620.22;
+        byte num2 = 127;
+
     }
 }
